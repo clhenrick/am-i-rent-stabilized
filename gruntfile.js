b1d1a3745009c435b66948e1e3e983fa36cd4866
Chris Henrick <chrishenrick@gmail.com>
2014-12-04 13:26:07 -0500

    update css styles

--- gruntfile.js
+++ gruntfile.js
@@ -1,19 +1,50 @@
 module.exports = function (grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        compass: {
+              dev: {
+                options: {
+                  config: 'config.rb',
+                  force: true
+                }
+              }
+        },
         autoprefixer: {
             dist: {
                 files: {
-                    'build/styles.css': 'public/css/styles.css'
+                    'build/styles.css': 'css/styles.css'
                 }
             }
         },
         watch: {
-            styles: {
-                files: ['public/css/styles.css'],
+          sass: {
+            files: ['assets/sass/**/*.scss'],
+            tasks: ['compass:dev']
+          },
+          /* watch and see if our javascript files change, or new packages are installed */
+          styles: {
+                files: ['css/styles.css'],
                 tasks: ['autoprefixer']
+          },
+          js: {
+            files: ['assets/js/main.js', 'components/**/*.js'],
+            tasks: ['uglify']
+          },
+          /* watch our files for change, reload */
+          livereload: {
+            files: ['*.html', 'assets/css/*.css', 'assets/images/*', 'assets/js/{main.min.js, plugins.min.js}'],
+            options: {
+              livereload: true
             }
+          },
         }
+
     });
+    grunt.registerTask('default', 'watch');
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-watch');
 };
