c847f5de0bc83cc8eb2581b8cbe52ebbe0d715f1
Chris Henrick <chrishenrick@gmail.com>
2015-04-10 00:15:29 -0400

    fix bugs with firefox

--- js/app.ui.no-jquery.js
+++ js/app.ui.no-jquery.js
@@ -508,7 +508,7 @@ app.ui = (function(w,d){
     w.addtocalendar.load();
     // change the text of the link to be more descriptive
     var atcLink = d.querySelector('#atc_text_link_link.atcb-link');
-    atcLink.innerText = "Click here to add a calendar reminder.";
+    atcLink.innerHTML = "Click here to create a calendar reminder.";
     atcLink.tabindex = "-1";
   }
 
