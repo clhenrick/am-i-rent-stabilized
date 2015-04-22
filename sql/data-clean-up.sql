2e121ce7f6d4e7d19914567df265283ff0251eb9
Chris L Henrick <chrishenrick@gmail.com>
2015-04-22 19:31:22 -0400

    update data processing

--- sql/data-clean-up.sql
+++ sql/data-clean-up.sql
@@ -1,13 +1,15 @@
+-- create a column for borough names
+ALTER TABLE nyc_likely_rent_stablized ADD COLUMN "borough" text;
+
 -- create actual borough names from abbreviations:
-UPDATE nyc_likely_rent_stabilized set boro_name_long = 'Manhattan' where borough = 'MN';
-
-UPDATE nyc_likely_rent_stabilized set boro_name_long = 'Brooklyn' where borough = 'BK';
-
-UPDATE nyc_likely_rent_stabilized set boro_name_long = 'Queens' where borough = 'QN';
-
-UPDATE nyc_likely_rent_stabilized set boro_name_long = 'Staten Island' where borough = 'SI';
-
-UPDATE nyc_likely_rent_stabilized set boro_name_long = 'Bronx' where borough = 'BX'
+UPDATE nyc_likely_rent_stablized
+SET borough = 
+	CASE WHEN borocode = 1 THEN 'Manhattan'
+		WHEN borocode = 2 THEN 'Bronx'
+		WHEN borocode = 3 THEN 'Brooklyn'
+		WHEN borocode = 4 THEN 'Queens'
+		WHEN borocode = 5 THEN 'Staten Island'
+	END
 
 -- sample ST_Intersection using a lat lon point:
 SELECT * FROM bk_likely_rent_stablized 
