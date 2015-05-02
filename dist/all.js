6dde5af8a8542d0869060086af595f2b024dd6f8
Chris L Henrick <chrishenrick@gmail.com>
2015-05-02 17:52:14 -0400

    translate main page

--- dist/all.js
+++ dist/all.js
@@ -498,7 +498,7 @@ app.f = (function(w,d) {
       w.addtocalendar.load();
       // change the text of the link to be more descriptive
       var atcLink = d.querySelector('#atc_text_link_link.atcb-link');
-      atcLink.innerHTML = "Create a calendar reminder.";
+      // atcLink.innerHTML = "Create a calendar reminder.";
       atcLink.tabindex = "-1";
     }
   };
