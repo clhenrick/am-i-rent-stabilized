4b11a296d3f4ebcb3374edcc01046700ef5ef91e
Chris L Henrick <chrishenrick@gmail.com>
2015-05-13 22:56:04 -0400

    load tiles over https

--- js/app/app.map.js
+++ js/app/app.map.js
@@ -236,7 +236,7 @@ app.map = (function(d,w,a,H,$){
     if (app.el.map.tap) app.el.map.tap.disable();
     // app.el.map.style.cursor='default';
 
-    var basemap = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
+    var basemap = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
     });
 
