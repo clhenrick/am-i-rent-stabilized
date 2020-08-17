6556cf441da637f3aa02e680268c3c8af2c62f8f
Chris Henrick <chrishenrick@gmail.com>
2020-08-17 09:58:59 -0700

    code formatting

--- app/js/app/app.init.js
+++ app/js/app/app.init.js
@@ -1,29 +1,27 @@
 var app = app || {};
 
-app.init = (function(w,d){
-  
+app.init = (function (w, d) {
   // gets the whole thing going
   // called after langToggle loads JSON data for content
-  function init(){
-      app.el = app.elems.refDOM();
-      app.a = app.address.address();
-      app.f = app.helpers.registerfns();
-      app.l.listen();
-      app.language.initLangButtons();
+  function init() {
+    app.el = app.elems.refDOM();
+    app.a = app.address.address();
+    app.f = app.helpers.registerfns();
+    app.l.listen();
+    app.language.initLangButtons();
 
-      app.events.publish('state-change', {
-        currentSlide : app.el.slides[0]
-      });
+    app.events.publish("state-change", {
+      currentSlide: app.el.slides[0],
+    });
 
-      app.f.onResize();
-      app.f.goToSlide(app.el.currentSlide);
-      app.a.createMailTo();
-      app.f.addToCalendar();      
-      app.map.init();      
+    app.f.onResize();
+    app.f.goToSlide(app.el.currentSlide);
+    app.a.createMailTo();
+    app.f.addToCalendar();
+    app.map.init();
   }
-  
-  return {
-    init : init
-  };
 
+  return {
+    init: init,
+  };
 })(window, document);
