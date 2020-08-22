0b6c0582abe0144d581f08967349bcfc3e75eb5c
Chris Henrick <chrishenrick@gmail.com>
2020-08-22 13:49:11 -0700

    gulpfile2 formatting

--- gulpfile2.babel.js
+++ gulpfile2.babel.js
@@ -1,20 +1,20 @@
 /*jshint esversion: 6 */
-import gulp from 'gulp';
+import gulp from "gulp";
 
-import jshint from 'gulp-jshint';
-import sass from 'gulp-sass';
-import concat from 'gulp-concat';
-import uglify from 'gulp-uglify';
-import rename from 'gulp-rename';
-import server from 'gulp-webserver';
-import plumber from 'gulp-plumber';
-import minifyCSS from 'gulp-minify-css';
-import handlebars from 'gulp-handlebars';
-import wrap from 'gulp-wrap';
-import declare from 'gulp-declare';
-import useref from 'gulp-useref';
-import gulpIf from 'gulp-if';
-import del from 'del';
+import jshint from "gulp-jshint";
+import sass from "gulp-sass";
+import concat from "gulp-concat";
+import uglify from "gulp-uglify";
+import rename from "gulp-rename";
+import server from "gulp-webserver";
+import plumber from "gulp-plumber";
+import minifyCSS from "gulp-minify-css";
+import handlebars from "gulp-handlebars";
+import wrap from "gulp-wrap";
+import declare from "gulp-declare";
+import useref from "gulp-useref";
+import gulpIf from "gulp-if";
+import del from "del";
 
 const sourcemaps = require("gulp-sourcemaps");
 import source from "vinyl-source-stream";
@@ -23,31 +23,31 @@ import browserify from "browserify";
 import babel from "babelify";
 
 const server_config = {
-  host: 'localhost',
-  port: '8000'
+  host: "localhost",
+  port: "8000",
 };
 
 const paths = {
   js: {
     watchFiles: "app/js/refactored/*.js",
     source: ["app/js/refactored/main.js"],
-    destMapFolder: ["dist/"]
+    destMapFolder: ["dist/"],
   },
   scss: {
     watchFiles: "app/scss/*.scss",
     source: ["app/scss/main.scss"],
-    destMapFolder: ["dist/"]
+    destMapFolder: ["dist/"],
   },
   build: {
     destBuildFolder: "dist",
     destMinCSSFileName: "bundle.min.css",
-    destMinJSFileName: "bundle.min.js"
-  }
+    destMinJSFileName: "bundle.min.js",
+  },
 };
 
 function handleError(err) {
   console.log(err.toString());
-  this.emit('end');
+  this.emit("end");
 }
 
 // delete all files in build
@@ -55,9 +55,16 @@ function clean() {
   return del([`${paths.build.destBuildFolder}/*`]);
 }
 
+/* HANDLE JAVASCRIPTS */
 function js() {
-  const bundler = browserify({ entries: paths.js.source }, { debug: true }).transform(babel);
-  return bundler.bundle()
+  const bundler = browserify(
+    { entries: paths.js.source },
+    {
+      ignoreTransform: ["app/js/refactored/templates.js"],
+    }
+  ).transform(babel);
+  return bundler
+    .bundle()
     .on("error", handleError)
     .pipe(source(paths.build.destMinJSFileName))
     .pipe(buffer())
