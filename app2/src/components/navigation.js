1fe6fa287975c3f2d7534267f02f02c22a1cdcbf
Chris Henrick <henrick@google.com>
2020-09-03 13:36:59 -0700

    added component class for nav menu toggle

--- app2/src/components/navigation.js
+++ app2/src/components/navigation.js
@@ -7,3 +7,28 @@ export function navigationMenuToggle() {
     mainNavList.classList.toggle("responsive");
   });
 }
+
+export class NavMenuToggle {
+  constructor() {
+    this.navIcon = document.querySelector(".burger");
+    this.mainNavList = document.querySelector(".main-nav ul");
+    this.bindEvents = this.bindEvents.bind(this);
+    this.removeEvents = this.removeEvents.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  bindEvents() {
+    console.log(this.bindEvents);
+    this.navIcon.addEventListener("click", this.handleClick);
+  }
+
+  removeEvents() {
+    this.navIcon.removeEventListener("click", this.handleClick);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    this.navIcon.classList.toggle("open");
+    this.mainNavList.classList.toggle("responsive");
+  }
+}
