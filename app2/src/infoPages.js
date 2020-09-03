c379a15cec2efe788ea08fb9e316b47723ae8ceb
Chris Henrick <henrick@google.com>
2020-09-03 13:37:45 -0700

    infoPages entry point uses NavMenuToggle component class

--- app2/src/infoPages.js
+++ app2/src/infoPages.js
@@ -1,9 +1,14 @@
 import "./scss/main.scss";
 import { langToggle } from "./utils/translate";
-import { navigationMenuToggle } from "./components/navigation";
+import { NavMenuToggle } from "./components/navigation";
 
 console.log("infoPages entry");
 
+function init() {
+  const navMenuToggle = new NavMenuToggle();
+  navMenuToggle.bindEvents();
+}
+
 // FIXME: navigationMenuToggle is only called once,
 // if language changes it needs to be called again.
-langToggle(navigationMenuToggle);
+langToggle(init);
