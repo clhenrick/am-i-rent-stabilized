749051bc93cef9d0e7c6910c99951674fcecf876
Chris Henrick <chrishenrick@gmail.com>
2020-09-04 11:58:18 -0700

    updated languageToggle component: - renamed changeLangButtons to setLanguageToggleBtns - rm use of jQuery - renamed getCurLang to getLangFromBtn - invoke setLanguageToggleBtns on init()

--- app2/src/components/languageToggle.js
+++ app2/src/components/languageToggle.js
@@ -1,5 +1,6 @@
 import { Component } from "./_componentBase";
-import { langToggle } from "../utils/translate";
+import { langToggle, getCurLang } from "../utils/translate";
+import { LANGS, IN_LANG } from "../utils/constants";
 
 export class LanguageToggle extends Component {
   constructor() {
@@ -7,10 +8,13 @@ export class LanguageToggle extends Component {
   }
 
   init() {
+    this.toggleBtnES = this.element.querySelector(".toggle-es");
+    this.toggleBtnZH = this.element.querySelector(".toggle-zh");
     this.handleClick = this.handleClick.bind(this);
-    this.getCurrentLanguage = this.getCurrentLanguage.bind(this);
-    this.changeLanguageButtons = this.changeLanguageButtons.bind(this);
+    this.getLangFromBtn = this.getLangFromBtn.bind(this);
+    this.setLanguageToggleBtns = this.setLanguageToggleBtns.bind(this);
     this.bindEvents();
+    this.setLanguageToggleBtns();
   }
 
   bindEvents() {
@@ -25,11 +29,17 @@ export class LanguageToggle extends Component {
     });
   }
 
-  getCurrentLanguage(target) {
+  handleClick(event) {
+    event.preventDefault();
+    window.localStorage.setItem("lang", this.getLangFromBtn(event.target));
+    langToggle();
+  }
+
+  getLangFromBtn(btn) {
     let result;
-    if (target.innerHTML === "en español") {
+    if (btn.innerHTML === "en español") {
       result = "es";
-    } else if (target.innerHTML === "中文") {
+    } else if (btn.innerHTML === "中文") {
       result = "zh";
     } else {
       result = "en";
@@ -38,39 +48,63 @@ export class LanguageToggle extends Component {
   }
 
   // TODO:
-  // - this should fire when template data has finished loading
   // - refactor logic?
-  // - drop usage of jQuery
-  changeLanguageButtons(lang) {
-    const $es = $(".lang-toggle .toggle-es");
-    const $zh = $(".lang-toggle .toggle-zh");
+  setLanguageToggleBtns() {
+    if (getCurLang() === LANGS.ES) {
+      // toggle lang to es
+      // 1. toggle previous lang text & lang attr to en
+      this.toggleBtnES.innerHTML = IN_LANG.EN;
+      this.toggleBtnES.lang = LANGS.EN;
 
-    if (lang === "es") {
-      $es.html("in english");
-      $es.removeClass("toggle-es").addClass("toggle-en");
-      $zh.html("中文");
-      $("body").addClass("es");
-      $("body").removeClass("en");
-      $("body").removeClass("zh");
-    } else if (lang === "zh") {
-      $es.html("en español");
-      $zh.html("in english");
-      $zh.removeClass("toggle-zh").addClass("toggle-en");
-      $("body").addClass("zh");
-      $("body").removeClass("es");
-      $("body").removeClass("en");
+      // 2. toggle class name to en
+      this.toggleBtnES.classList.remove("toggle-es");
+      this.toggleBtnES.classList.add("toggle-en");
+
+      // 3. make sure other button has correct text & lang attr
+      this.toggleBtnZH.innerHTML = IN_LANG.ZH;
+      this.toggleBtnZH.lang = LANGS.ZH;
+
+      // 4. update body class name to current lang
+      document.body.classList.add("es");
+      document.body.classList.remove("en");
+      document.body.classList.remove("zh");
+
+      // 5. set lang attr on html tag
+      document.querySelector("html").lang = LANGS.ES;
+    } else if (getCurLang() === LANGS.ZH) {
+      // toggle to zh
+      // similar process to toggle to es above
+      this.toggleBtnES.innerHTML = IN_LANG.ES;
+      this.toggleBtnES.lang = LANGS.ES;
+
+      this.toggleBtnZH.innerHTML = IN_LANG.EN;
+      this.toggleBtnZH.lang = LANGS.EN;
+
+      this.toggleBtnZH.classList.remove("toggle-zh");
+      this.toggleBtnZH.classList.add("toggle-en");
+
+      document.body.classList.add("zh");
+      document.body.classList.remove("es");
+      document.body.classList.remove("en");
+
+      document.querySelector("html").lang = LANGS.ZH;
     } else {
-      $es.html("en español");
-      $zh.html("中文");
-      $("body").addClass("en");
-      $("body").removeClass("es");
-      $("body").removeClass("zh");
-    }
-  }
+      // default is to set lang to en
 
-  handleClick(event) {
-    event.preventDefault();
-    window.localStorage.setItem("lang", this.getCurrentLanguage(event.target));
-    langToggle();
+      // make sure other btns text & lang attr are correctly set
+      this.toggleBtnES.innerHTML = IN_LANG.ES;
+      this.toggleBtnES.lang = LANGS.ES;
+
+      this.toggleBtnZH.innerHTML = IN_LANG.ZH;
+      this.toggleBtnZH.lang = LANGS.ZH;
+
+      // toggle body class name
+      document.body.classList.add("en");
+      document.body.classList.remove("es");
+      document.body.classList.remove("zh");
+
+      // set html tag's lang attr
+      document.querySelector("html").lang = LANGS.EN;
+    }
   }
 }
