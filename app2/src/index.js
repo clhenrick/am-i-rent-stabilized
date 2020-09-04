5dcc8932a4604039f8d0d80621b4e282ecf9e032
Chris Henrick <chrishenrick@gmail.com>
2020-09-04 10:59:58 -0700

    cleaned up index.js & infoPages.js entry point modules

--- app2/src/index.js
+++ app2/src/index.js
@@ -1,14 +1,7 @@
 /*jshint esversion: 6 */
 import "./scss/main.scss";
 import { langToggle } from "./utils/translate";
-import { navigationMenuToggle } from "./components/navigation";
 
 console.log("index entry");
 
-// FIXME: navigationMenuToggle is only called once,
-// if language changes it needs to be called again.
-langToggle(handleContentLoaded);
-
-function handleContentLoaded() {
-  navigationMenuToggle();
-}
+langToggle();
--- app2/src/infoPages.js
+++ app2/src/infoPages.js
@@ -1,15 +1,6 @@
 import "./scss/main.scss";
 import { langToggle } from "./utils/translate";
-import { NavMenuToggle } from "./components/navigation";
-import { LanguageToggle } from "./components/languageToggle";
 
 console.log("infoPages entry");
 
-function init() {
-  const navMenuToggle = new NavMenuToggle();
-  const languageToggle = new LanguageToggle();
-}
-
-// FIXME: navigationMenuToggle is only called once,
-// if language changes it needs to be called again.
-langToggle(init);
+langToggle();
