4f4532f1e8e85eb4e7b5cc694076e67c4e81f3fd
Chris Henrick <chrishenrick@gmail.com>
2020-09-04 17:52:08 -0700

    rm dead code from navigation.js

--- app2/src/components/navigation.js
+++ app2/src/components/navigation.js
@@ -1,15 +1,5 @@
 import { Component } from "./_componentBase";
 
-export function navigationMenuToggle() {
-  const navIcon = document.querySelector(".burger");
-  const mainNavList = document.querySelector(".main-nav ul");
-  navIcon.addEventListener("click", function (e) {
-    e.preventDefault();
-    navIcon.classList.toggle("open");
-    mainNavList.classList.toggle("responsive");
-  });
-}
-
 export class NavMenuToggle extends Component {
   constructor(props) {
     super(props);
