9b487f8be53e3b42fb2452619703fb64804a0e68
Chris Henrick <chrishenrick@gmail.com>
2020-09-06 12:43:08 -0700

    refactored translate.js

--- app2/src/utils/translate.js
+++ app2/src/utils/translate.js
@@ -1,82 +1,11 @@
-/*jshint esversion: 6 */
+// FIXME: alias these in webpack config
 const w = window;
 const d = document;
 
 // loads the correct lang json & hbs template;
 // this gets called when the page first loads and when the user clicks the lang button
-export function langToggle() {
-  let currentPage = d.URL.substring(
-    d.URL.lastIndexOf("/") + 1,
-    d.URL.lastIndexOf(".")
-  );
-
-  // FIXME: redirect URL to index.html instead? or a 404 page?
-  if (["index", "why", "how", "resources"].indexOf(currentPage) === -1) {
-    currentPage = "index";
-  }
-
-  loadTemplateData(getCurLang(), currentPage);
-}
-
-export async function loadTemplateData(lang, currentPage) {
-  let template,
-    html,
-    filePath,
-    contentFolder = "data/";
-
-  // set variables to the correct JSON file & template based on the app's current page
-  if (currentPage === "index") {
-    filePath = contentFolder + "main-content.json";
-    const { default: _ } = await import(
-      /* webpackChunkName: "main-hbs" */ "../hbs_templates/main.hbs"
-    );
-    template = _;
-  } else if (currentPage === "why") {
-    filePath = "../" + contentFolder + "why-content.json";
-    const { default: _ } = await import(
-      /* webpackChunkName: "why-hbs" */ "../hbs_templates/why.hbs"
-    );
-    template = _;
-  } else if (currentPage === "how") {
-    filePath = "../" + contentFolder + "how-content.json";
-    const { default: _ } = await import(
-      /* webpackChunkName: "how-hbs" */ "../hbs_templates/how.hbs"
-    );
-    template = _;
-  } else if (currentPage === "resources") {
-    filePath = "../" + contentFolder + "resources-content.json";
-    const { default: _ } = await import(
-      /* webpackChunkName: "resources-hbs" */ "../hbs_templates/resources.hbs"
-    );
-    template = _;
-  }
-
-  $.getJSON(filePath, function (data) {
-    // load the correct language from the json data
-    if (lang === "es") {
-      html = template(data.languages.es);
-    } else if (lang === "zh") {
-      html = template(data.languages.zh);
-    } else {
-      html = template(data.languages.en);
-    }
-    d.querySelector("#wrapper").innerHTML = html;
-  }).done(async function () {
-    // load the correct chunk for either the app for index.html or helper js for info/*.html pages
-    let init;
-    if (currentPage === "index") {
-      const { default: _ } = await import(
-        /* webpackChunkName: "init-app" */ "./initApp.js"
-      );
-      init = _;
-    } else {
-      const { default: _ } = await import(
-        /* webpackChunkName: "init-info-pages" */ "./initInfoPages.js"
-      );
-      init = _;
-    }
-    init();
-  });
+export function translatePage() {
+  renderHtmlFromTemplate(getCurLang(), getCurrentPageName());
 }
 
 export function getCurLang() {
@@ -85,4 +14,49 @@ export function getCurLang() {
 
 export function setCurLang(lang) {
   w.localStorage.setItem("lang", lang || "en");
+}
+
+async function renderHtmlFromTemplate(lang, currentPage) {
+  try {
+    const { default: template } = await getHtmlTemplate(currentPage);
+    const { languages } = await getLocaleJson(lang, currentPage);
+    renderHtml(lang, languages, template);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function getCurrentPageName() {
+  let currentPage = d.URL.substring(
+    d.URL.lastIndexOf("/") + 1,
+    d.URL.lastIndexOf(".")
+  );
+  // FIXME: redirect URL to index.html instead? or a 404 page?
+  if (["index", "why", "how", "resources"].indexOf(currentPage) === -1) {
+    currentPage = "index";
+  }
+  return currentPage;
+}
+
+function renderHtml(lang, localeJson, template) {
+  if (lang && localeJson && template) {
+    d.querySelector("#wrapper").innerHTML = template(localeJson[lang]);
+  }
+}
+
+async function getLocaleJson(lang, currentPage) {
+  const localesDirName = "data";
+  const dir = currentPage === "index" ? "." : "..";
+  const name = currentPage === "index" ? "main" : currentPage;
+  const localeFileName = `${name}-content.json`;
+  const res = await fetch(`${dir}/${localesDirName}/${localeFileName}`);
+  if (res.ok) return res.json();
+  throw new Error("Problem fetching locale json");
+}
+
+async function getHtmlTemplate(pageName) {
+  const name = pageName === "index" ? "main" : pageName;
+  return await import(
+    /* webpackChunkName: "locales-hbs" */ `../hbs_templates/${name}.hbs`
+  );
 }
