749ac413a4532461af8a02213deea38daef28332
Chris Henrick <chrishenrick@gmail.com>
2020-09-14 11:41:39 -0700

    updated LanguageToggleButton class tests: - check for errors when missing required props

--- app2/src/components/languageToggleButton.spec.js
+++ app2/src/components/languageToggleButton.spec.js
@@ -2,14 +2,16 @@ import { LanguageToggleButton } from "./languageToggleButton";
 
 describe("LanguageToggleButton", () => {
   let es;
+  let element;
 
   beforeAll(() => {
     document.body.innerHTML =
       "<a class='toggle-es' lang='es' href='#'>en español</a>";
+    element = document.querySelector("a.toggle-es");
     es = new LanguageToggleButton({
       lang: "es",
       label: "en español",
-      element: document.querySelector("a.toggle-es"),
+      element,
     });
   });
 
@@ -19,6 +21,43 @@ describe("LanguageToggleButton", () => {
 
   test("The consumer should be able to call new() on LanguageToggleButton", () => {
     expect(es).toBeTruthy();
+  });
+
+  test("It throws an Error if not passed a valid `lang` property", () => {
+    const errorMsg = "LanguageToggleButton requires a valid `lang` property";
+
+    try {
+      new LanguageToggleButton({ element });
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error.message).toBe(errorMsg);
+    }
+
+    try {
+      new LanguageToggleButton({ element, lang: 0 });
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error.message).toBe(errorMsg);
+    }
+  });
+
+  test("It throws an Error if not passed a valid `label` property", () => {
+    const errorMsg = "LanguageToggleButton requires a valid `label` property";
+    const lang = "zh";
+
+    try {
+      new LanguageToggleButton({ element, lang });
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error.message).toBe(errorMsg);
+    }
+
+    try {
+      new LanguageToggleButton({ element, lang, label: NaN });
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error.message).toBe(errorMsg);
+    }
   });
 
   test("It should have the correct className property", () => {
