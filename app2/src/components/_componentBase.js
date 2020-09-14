20637853a179c806d938306689f5e9176c65e394
Chris Henrick <chrishenrick@gmail.com>
2020-09-14 09:07:53 -0700

    updated Component class: - explicity set methods on class body - stricter check for element prop

--- app2/src/components/_componentBase.js
+++ app2/src/components/_componentBase.js
@@ -1,20 +1,25 @@
 export class Component {
   constructor(props = {}) {
-    this.init = this.init || function () {};
     this.init = this.init.bind(this);
-
-    this.bindEvents = this.bindEvents || function () {};
     this.bindEvents = this.bindEvents.bind(this);
-
-    this.removeEvents = this.removeEvents || function () {};
     this.removeEvents = this.removeEvents.bind(this);
 
-    if ("element" in props) {
+    if ("element" in props && props.element instanceof HTMLElement) {
       this.element = props.element;
+    } else {
+      throw new Error("Component requires a valid DOM element prop");
     }
 
-    // do other setup work here such as add more class properties, fetch data, etc.
-    // make sure to call this.bindEvents() here
     this.init();
   }
+
+  // Do any other setup work here such as add more class properties,
+  // fetch data, etc. Make sure to call this.bindEvents() here
+  init() {}
+
+  // Add any DOM event listeners
+  bindEvents() {}
+
+  // Remove any DOM event listeners
+  removeEvents() {}
 }
