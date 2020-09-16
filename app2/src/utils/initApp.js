2cfab31a7455d8597c0dea4fca5b65b7e33bcad4
Chris Henrick <chrishenrick@gmail.com>
2020-09-16 10:06:06 -0700

    initApp creates multiple instances of AdvanceSlides

--- app2/src/utils/initApp.js
+++ app2/src/utils/initApp.js
@@ -1,12 +1,21 @@
 import { NavMenuToggle } from "../components/navigation";
 import { LanguageToggle } from "../components/languageToggle";
+import { AdvanceSlides } from "../components/advanceSlides";
 
 export default function initApp() {
+  // nav menu hamburger icon
   new NavMenuToggle({ element: document.querySelector("nav.main-nav") });
+
+  // language toggle desktop & mobile
   new LanguageToggle({
     element: document.querySelector("div.desktop > div.lang-toggle"),
   });
   new LanguageToggle({
     element: document.querySelector("div.mobile > div.lang-toggle"),
+  });
+
+  // slide advance buttons
+  document.querySelectorAll(".go-next.bottom-arrow").forEach((element) => {
+    new AdvanceSlides({ element });
   });
 }
