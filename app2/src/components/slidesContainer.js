10114b84f22cb7a6a082b9262c57a70ee9ec1f2b
Chris Henrick <chrishenrick@gmail.com>
2020-09-17 11:21:40 -0700

    Updated SlidesContainer to handle slides scrolling

--- app2/src/components/slidesContainer.js
+++ app2/src/components/slidesContainer.js
@@ -7,14 +7,45 @@ export class SlidesContainer extends Component {
   }
 
   init() {
+    this.slides = [...this.element.querySelectorAll(".slide")];
+
     this.handleSlidesUpdate = this.handleSlidesUpdate.bind(this);
+    this.scrollToActiveSlide = this.scrollToActiveSlide.bind(this);
+
+    this.activeSlide = store.getState().slides.curIndex;
     this.handleSlidesUpdate();
   }
 
   handleSlidesUpdate() {
     store.subscribe(() => {
       const { slides } = store.getState();
-      console.log("Slides state update: ", slides);
+      if (slides.curIndex !== this.activeSlideIdx) {
+        console.log("activeSlide: ", this.activeSlide);
+        console.log(`go to slide ${slides.curIndex}`);
+        this.activeSlide = slides.curIndex;
+        this.scrollToActiveSlide();
+      }
     });
+  }
+
+  scrollToActiveSlide() {
+    this.activeSlide.scrollIntoView({ behavior: "smooth" });
+  }
+
+  set activeSlide(value) {
+    this.slides.forEach((slide) => slide.classList.remove("active"));
+    this.slides[value].classList.add("active");
+  }
+
+  get activeSlide() {
+    return this.slides[this.activeSlideIdx];
+  }
+
+  set activeSlideIdx(value) {
+    this.activeSlide = value;
+  }
+
+  get activeSlideIdx() {
+    return this.slides.findIndex((slide) => slide.classList.contains("active"));
   }
 }
