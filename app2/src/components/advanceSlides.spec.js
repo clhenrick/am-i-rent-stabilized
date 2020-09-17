8f05ba2cc84b05a55b20b75f92746eaa64bcdfc5
Chris Henrick <chrishenrick@gmail.com>
2020-09-17 12:44:56 -0700

    added tests for AdvanceSlides use of redux store

--- app2/src/components/advanceSlides.spec.js
+++ app2/src/components/advanceSlides.spec.js
@@ -1,4 +1,20 @@
 import { AdvanceSlides } from "./advanceSlides";
+import { store } from "../store";
+
+jest.mock("../store", () => {
+  return {
+    __esModule: true,
+    store: {
+      getState: jest.fn(() => ({
+        slides: {
+          curIndex: 0,
+          canAdvance: true,
+        },
+      })),
+      dispatch: jest.fn(),
+    },
+  };
+});
 
 describe("AdvanceSlides", () => {
   let advanceSlides;
@@ -27,5 +43,23 @@ describe("AdvanceSlides", () => {
   test("The component's button handles a click event", () => {
     document.querySelector(".go-next.bottom-arrow > h3").click();
     expect(spyButton).toHaveBeenCalled();
+  });
+
+  test("handleClick method fires the nextSlide action creator", () => {
+    jest.clearAllMocks();
+    advanceSlides.handleClick();
+    expect(store.dispatch).toHaveBeenCalledWith({ type: "GoToNextSlide" });
+  });
+
+  test("handleClick method does not fire the nextSlide action creator", () => {
+    jest.clearAllMocks();
+    store.getState.mockImplementationOnce(() => ({
+      slides: {
+        curIndex: 0,
+        canAdvance: false,
+      },
+    }));
+    advanceSlides.handleClick();
+    expect(store.dispatch).not.toHaveBeenCalled();
   });
 });
