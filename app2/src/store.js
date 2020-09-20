9fda9029ac51458a03215d97bdc4f09271cd8a09
Chris Henrick <chrishenrick@gmail.com>
2020-09-19 18:36:20 -0700

    enable redux dev tools in dev env

--- app2/src/store.js
+++ app2/src/store.js
@@ -1,7 +1,16 @@
 import { createStore } from "redux";
 import { rootReducer } from "./reducers";
 
-export const store = createStore(rootReducer);
+let reduxDevTools;
+
+if (process.env.NODE_ENV !== "production") {
+  // enables Redux dev tools via the Chrome Browser Extension
+  reduxDevTools =
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+}
+
+export const store = createStore(rootReducer, undefined, reduxDevTools);
 
 // log initial state
 console.log("initial redux state: ", store.getState());
