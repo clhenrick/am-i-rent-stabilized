3da6666ff798a51d6748977da8bea7749fcdaa05
Chris Henrick <chrishenrick@gmail.com>
2020-09-24 17:37:25 -0700

    AddressSearchForm integrates with redux store
    listens to state.addressGeocode updates
    dispatches async action addressGeocodeFetch on input event

--- app/src/components/addressSearchForm.js
+++ app/src/components/addressSearchForm.js
@@ -1,4 +1,6 @@
 import { Component } from "./_componentBase";
+import { addressGeocodeFetch } from "../action_creators";
+import { store } from "../store";
 
 export class AddressSearchForm extends Component {
   constructor(props) {
@@ -17,7 +19,11 @@ export class AddressSearchForm extends Component {
     this.removeEvents = this.removeEvents.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleStoreSubscribtion = this.handleStoreSubscribtion.bind(this);
+    this.handleGeocodeResponse = this.handleGeocodeResponse.bind(this);
+    this.handleGeocodeError = this.handleGeocodeError.bind(this);
 
+    store.subscribe(this.handleStoreSubscribtion);
     this.bindEvents();
   }
 
@@ -37,6 +43,31 @@ export class AddressSearchForm extends Component {
 
   handleInputChange(event) {
     this.addressSearchText = event.target.value;
-    console.log(this.addressSearchText);
+    // TODO: throttle text input as to not overwhelm the geocoding API
+    if (this.addressSearchText.length > 3) {
+      store.dispatch(addressGeocodeFetch(this.addressSearchText));
+    }
+  }
+
+  handleStoreSubscribtion() {
+    const {
+      addressGeocode: { result, status, error },
+    } = store.getState();
+    if (result) {
+      this.handleGeocodeResponse(result, status);
+    }
+    if (error) {
+      this.handleGeocodeError(error);
+    }
+  }
+
+  handleGeocodeResponse(data, status) {
+    // TODO handle updating form input with autocomplete results
+    console.log(data, status);
+  }
+
+  handleGeocodeError(error) {
+    // TODO: error handling
+    console.error(error);
   }
 }
