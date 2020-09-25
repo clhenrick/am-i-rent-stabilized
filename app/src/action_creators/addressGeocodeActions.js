38983b512e0340018b68804e7eecea2d79e38b22
Chris Henrick <chrishenrick@gmail.com>
2020-09-24 17:33:26 -0700

    added async action creator for address geocoding
    uses the geocoding endpoint provided by NYC Planning Labs
    see: https://labs-geosearch-docs.netlify.app/docs/#autocomplete

--- app/src/action_creators/addressGeocodeActions.js
+++ app/src/action_creators/addressGeocodeActions.js
@@ -1,3 +1,5 @@
+import fetch from "cross-fetch";
+
 import {
   AddressGeocodeRequest,
   AddressGeocodeSuccess,
@@ -17,3 +19,23 @@ export const addressGeocodeFailure = (error) => ({
   type: AddressGeocodeFailure,
   error,
 });
+
+export const addressGeocodeFetch = (text) => (dispatch) => {
+  dispatch(addressGeocodeRequest);
+  return fetch(
+    `https://geosearch.planninglabs.nyc/v1/autocomplete?text=${text}`
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Problem fetching geosearch data");
+      }
+    })
+    .then((json) => {
+      dispatch(addressGeocodeSuccess(json));
+    })
+    .catch((error) => {
+      dispatch(addressGeocodeFailure(error));
+    });
+};
