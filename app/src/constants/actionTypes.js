ea765686b28c0987faa32a7f343a2f8db6665826
Chris Henrick <chrishenrick@gmail.com>
2020-09-29 09:26:08 -0700

    added new action types for address geocode: - differentiate search result from autosuggestions

--- app/src/constants/actionTypes.js
+++ app/src/constants/actionTypes.js
@@ -4,7 +4,17 @@ export const GoToPrevSlide = "GoToPrevSlide";
 export const GoToSlideIdx = "GoToSlideIdx";
 export const CanAdvanceSlide = "CanAdvanceSlide";
 
-// ... for address geocode
+// to remove...
 export const AddressGeocodeRequest = "AddressGeocodeRequest";
 export const AddressGeocodeSuccess = "AddressGeocodeSuccess";
 export const AddressGeocodeFailure = "AddressGeocodeFailure";
+
+// ... for address geocode seach result
+export const AddressSearchRequest = "AddressSearchRequest";
+export const AddressSearchSuccess = "AddressSearchSuccess";
+export const AddressSearchFailure = "AddressSearchFailure";
+
+// ... for address geocode autosuggestions
+export const AddressAutosuggestRequest = "AddressAutosuggestRequest";
+export const AddressAutosuggestSuccess = "AddressAutosuggestSuccess";
+export const AddressAutosuggestFailure = "AddressAutosuggestFailure";
