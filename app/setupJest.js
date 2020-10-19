2c74df9f033af54694aee134d4b6cf2261e1346e
Chris Henrick <chrishenrick@gmail.com>
2020-10-18 19:45:40 -0700

    updated setupJest - format coded - register handlebars partial for SearchResultMap

--- app/setupJest.js
+++ app/setupJest.js
@@ -1,4 +1,4 @@
-require('jest-fetch-mock').enableMocks();
+require("jest-fetch-mock").enableMocks();
 
 const fs = require("fs");
 const path = require("path");
@@ -9,7 +9,7 @@ registerHbsPartials();
 
 global.setDocumentHtml = (html) => {
   document.body.innerHTML = `<div id="wrapper">${html}</div>`;
-}
+};
 
 global.getMainHtml = () => {
   const localeData = require(`./public/${LOCALES_JSON_DIR}/main-en.json`);
@@ -19,33 +19,56 @@ global.getMainHtml = () => {
   );
   const template = Handlebars.compile(hbsFile);
   return template(localeData);
-}
+};
 
 function registerHbsPartials() {
   // these partials are automatically read by webpack using the handlebars-loader
   // for Jest we have to register them separately
-  Handlebars.registerPartial("navigation", fs.readFileSync(
-    path.resolve(__dirname, "./src/hbs_partials/navigation.hbs"),
-    "utf8"
-  ));
+  Handlebars.registerPartial(
+    "navigation",
+    fs.readFileSync(
+      path.resolve(__dirname, "./src/hbs_partials/navigation.hbs"),
+      "utf8"
+    )
+  );
 
-  Handlebars.registerPartial("navigation_info", fs.readFileSync(
-    path.resolve(__dirname, "./src/hbs_partials/navigation_info.hbs"),
-    "utf8"
-  ));
+  Handlebars.registerPartial(
+    "navigation_info",
+    fs.readFileSync(
+      path.resolve(__dirname, "./src/hbs_partials/navigation_info.hbs"),
+      "utf8"
+    )
+  );
 
-  Handlebars.registerPartial("language_toggle", fs.readFileSync(
-    path.resolve(__dirname, "./src/hbs_partials/language_toggle.hbs"),
-    "utf8"
-  ));
+  Handlebars.registerPartial(
+    "language_toggle",
+    fs.readFileSync(
+      path.resolve(__dirname, "./src/hbs_partials/language_toggle.hbs"),
+      "utf8"
+    )
+  );
 
-  Handlebars.registerPartial("progress_indicator", fs.readFileSync(
-    path.resolve(__dirname, "./src/hbs_partials/progress_indicator.hbs"),
-    "utf8"
-  ));
+  Handlebars.registerPartial(
+    "progress_indicator",
+    fs.readFileSync(
+      path.resolve(__dirname, "./src/hbs_partials/progress_indicator.hbs"),
+      "utf8"
+    )
+  );
 
-  Handlebars.registerPartial("address_search_form", fs.readFileSync(
-    path.resolve(__dirname, "./src/hbs_partials/address_search_form.hbs"),
-    "utf8"
-  ));
+  Handlebars.registerPartial(
+    "address_search_form",
+    fs.readFileSync(
+      path.resolve(__dirname, "./src/hbs_partials/address_search_form.hbs"),
+      "utf8"
+    )
+  );
+
+  Handlebars.registerPartial(
+    "search_result_map",
+    fs.readFileSync(
+      path.resolve(__dirname, "./src/hbs_partials/search_result_map.hbs"),
+      "utf8"
+    )
+  );
 }
