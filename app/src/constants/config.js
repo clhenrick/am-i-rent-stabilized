544ada8291088cae55c47b7b16244b7b5626423f
Chris Henrick <chrishenrick@gmail.com>
2020-10-18 19:39:00 -0700

    added config constant for carto maps api uri

--- app/src/constants/config.js
+++ app/src/constants/config.js
@@ -1,3 +1,4 @@
 // CARTO account config related
 export const cartoAccount = "chenrick";
 export const rentStabilizedTable = "map_pluto_likely_rs_2016v1";
+export const mapsApiUrl = `https://${cartoAccount}.carto.com/api/v1/map/`;
