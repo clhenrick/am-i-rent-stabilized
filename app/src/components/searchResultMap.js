ef84b80411ed4d74adcfa1c23012b2b2b5573021
Chris Henrick <chrishenrick@gmail.com>
2020-10-19 20:31:06 -0700

    updated SearchResultMap:
    added/updated getters & setters for projection, zoom, center
    handle rendering of basemap tiles

--- app/src/components/searchResultMap.js
+++ app/src/components/searchResultMap.js
@@ -3,6 +3,14 @@ import { observeStore } from "../store";
 import { mapsApiSql } from "../utils/sql";
 const d3 = Object.assign({}, require("d3-tile"), require("d3-geo"));
 
+const ZOOM = {
+  DEFAULT: 20,
+  RESULT: 26,
+};
+const CENTER = {
+  DEFAULT: [-74.006, 40.7128],
+};
+
 export class SearchResultMap extends Component {
   constructor(props) {
     super(props);
@@ -11,21 +19,33 @@ export class SearchResultMap extends Component {
   init() {
     super.checkForStore();
 
+    this.svg = this.element.querySelector("svg");
+    this.gBaseTiles = this.svg.querySelector("g.tiles-base-map");
+    this.gRsTiles = this.svg.querySelector("g.tiles-rent-stabilized");
+    this.marker = this.svg.querySelector("symbol");
+
     this.width = 550;
     this.height = 300;
-    this.zoom = 20;
+
+    this._zoom = ZOOM.DEFAULT;
+    this._center = CENTER.DEFAULT;
     this._projection = d3.geoMercator();
 
+    this.updateProjection = this.updateProjection.bind(this);
     this.updateMapView = this.updateMapView.bind(this);
     this.handleSearchResult = this.handleSearchResult.bind(this);
     this.renderMap = this.renderMap.bind(this);
-    this.tonerTileUrl = this.tonerTileUrl.bind(this);
+    this.renderTile = this.renderTile.bind(this);
+    this.getTonerTileUrl = this.getTonerTileUrl.bind(this);
 
     observeStore(
       this.store,
       (state) => state.addressGeocode.searchResult,
       this.handleSearchResult
     );
+
+    this.updateProjection();
+    this.renderMap();
   }
 
   handleSearchResult() {
@@ -50,18 +70,38 @@ export class SearchResultMap extends Component {
       zipcode,
     } = this.searchResultDetails;
     console.log(coordinates, name, borough, state, zipcode);
-    this.projection = { zoom: 26, center: coordinates };
+    this.zoom = ZOOM.RESULT;
+    this.center = coordinates;
     this.renderMap();
   }
 
-  renderMap() {}
+  renderMap() {
+    this.gBaseTiles.innerHTML = `${this.tiles.map(this.renderTile)}`;
+  }
 
-  tonerTileUrl(x, y, z) {
+  renderTile([x, y, z], i, { translate: [tx, ty], scale: k }) {
+    return `<image xlink:href="${this.getTonerTileUrl(
+      x,
+      y,
+      z
+    )}" x="${Math.round((x + tx) * k)}" y="${Math.round(
+      (y + ty) * k
+    )}" width="${k}" height="${k}"></image>`;
+  }
+
+  getTonerTileUrl(x, y, z) {
     return `https://stamen-tiles-${
       "abc"[Math.abs(x + y) % 3]
     }.a.ssl.fastly.net/toner-lite/${z}/${x}/${y}${
       devicePixelRatio > 1 ? "@2x" : ""
     }.png`;
+  }
+
+  updateProjection() {
+    this._projection
+      .center(this.center)
+      .scale(Math.pow(2, this.zoom) / (2 * Math.PI))
+      .translate([this.width / 2, this.height / 2]);
   }
 
   get searchResultDetails() {
@@ -90,16 +130,34 @@ export class SearchResultMap extends Component {
     return searchResult;
   }
 
-  get projection() {
-    return this._projection;
+  get zoom() {
+    return this._zoom;
   }
 
-  set projection({ center, zoom }) {
-    this._projection = d3
-      .geoMercator()
-      .center(center)
-      .scale(Math.pow(2, zoom) / (2 * Math.PI))
-      .translate([this.width / 2, this.height / 2]);
+  set zoom(value) {
+    if (value && typeof value === "number") {
+      this._zoom = value;
+      this.updateProjection();
+    } else {
+      throw "zoom should be an integer";
+    }
+  }
+
+  get center() {
+    return this._center;
+  }
+
+  set center(coords) {
+    if (Array.isArray(coords) && coords.length === 2) {
+      this._center = coords;
+      this.updateProjection();
+    } else {
+      throw "center must be an array with lat lon coordinates.";
+    }
+  }
+
+  get projection() {
+    return this._projection;
   }
 
   get tile() {
