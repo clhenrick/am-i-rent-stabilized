4d97923c87906c5e31cc60698faa6a4ccbd8f704
Chris Henrick <chrishenrick@gmail.com>
2020-10-25 17:21:03 -0700

    SearchResultMap updated tests

--- app/src/components/searchResultMap.spec.js
+++ app/src/components/searchResultMap.spec.js
@@ -76,18 +76,51 @@ describe("SearchResultMap", () => {
     ).toThrow("Requires redux store");
   });
 
-  test("getCartoTileUrl", () => {
+  test("getBasemapTileUrl", () => {
     window.devicePixelRatio = 2;
+
     searchResultMap = new SearchResultMap({
       element,
       store,
     });
-    expect(searchResultMap.getCartoTileUrl(40, 30, 20)).toEqual(
+
+    expect(searchResultMap.getBasemapTileUrl(40, 30, 20)).toEqual(
       "https://cartodb-basemaps-b.global.ssl.fastly.net/light_all/20/40/30@2x.png"
     );
+
     window.devicePixelRatio = 1;
-    expect(searchResultMap.getCartoTileUrl(1, 2, 3)).toEqual(
+    expect(searchResultMap.getBasemapTileUrl(1, 2, 3)).toEqual(
       "https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/3/1/2.png"
+    );
+  });
+
+  test("getDataTileUrl", () => {
+    window.devicePixelRatio = 2;
+
+    searchResultMap = new SearchResultMap({
+      element,
+      store,
+    });
+
+    searchResultMap.cartoTilesSchema = {
+      layergroupid: "blablabla",
+      cdn_url: {
+        templates: {
+          https: {
+            url: "https://cartocdn-gusc-{s}.global.ssl.fastly.net",
+            subdomains: ["a", "b", "c", "d"],
+          },
+        },
+      },
+    };
+
+    expect(searchResultMap.getDataTileUrl(40, 30, 20)).toEqual(
+      "https://cartocdn-gusc-c.global.ssl.fastly.net/chenrick/api/v1/map/blablabla/20/40/30@2x.png"
+    );
+
+    window.devicePixelRatio = 1;
+    expect(searchResultMap.getDataTileUrl(1, 2, 3)).toEqual(
+      "https://cartocdn-gusc-d.global.ssl.fastly.net/chenrick/api/v1/map/blablabla/3/1/2.png"
     );
   });
 });
