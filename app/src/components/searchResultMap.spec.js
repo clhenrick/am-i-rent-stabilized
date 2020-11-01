5a6628b6abfd5a81c9db2c2bcfd6ba32aaadf958
Chris Henrick <chrishenrick@gmail.com>
2020-10-31 21:45:14 -0700

    updated tests for SearchResultMap

--- app/src/components/searchResultMap.spec.js
+++ app/src/components/searchResultMap.spec.js
@@ -75,4 +75,44 @@ describe("SearchResultMap", () => {
         })
     ).toThrow("Requires redux store");
   });
+
+  test("setPopupContent to empty", () => {
+    searchResultMap.popup.innerHTML = "<p></p>";
+    searchResultMap.setPopupContent();
+    expect(searchResultMap.popup.innerHTML).toEqual("");
+  });
+
+  test("setPopupContent to not empty", () => {
+    searchResultMap.setPopupContent({
+      name: "999 West St",
+      borough: "Brooklyn",
+      state: "NY",
+      zipcode: "99999",
+    });
+    expect(searchResultMap.popup.innerHTML).toEqual(
+      "<p>999 West St</p><p>Brooklyn NY 99999</p>"
+    );
+  });
+
+  test("showPopUp", () => {
+    searchResultMap.hidePopUp();
+    searchResultMap.showPopUp();
+    expect(searchResultMap.popup.classList.contains("hidden")).toBe(false);
+  });
+
+  test("hidePopUp", () => {
+    searchResultMap.hidePopUp();
+    expect(searchResultMap.popup.classList.contains("hidden")).toBe(true);
+  });
+
+  test("showMarker", () => {
+    searchResultMap.hideMarker();
+    searchResultMap.showMarker();
+    expect(searchResultMap.marker.getAttribute("opacity")).toBe("1");
+  });
+
+  test("hideMarker", () => {
+    searchResultMap.hideMarker();
+    expect(searchResultMap.marker.getAttribute("opacity")).toBe("0");
+  });
 });
