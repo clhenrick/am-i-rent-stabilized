7c6208bfcfc9824aba840981472ad31690663275
Chris Henrick <chrishenrick@gmail.com>
2020-11-01 20:26:43 -0800

    AddToCalendarButton sets start & end date time using dayjs

--- app/src/components/addToCalendarBtn.js
+++ app/src/components/addToCalendarBtn.js
@@ -1,6 +1,7 @@
+import dayjs from "dayjs";
 import { Component } from "./_componentBase";
 
-// wrapper component for add-to-calendar-button widget
+// Wrapper component for add-to-calendar-button widget
 // https://www.addevent.com/add-to-calendar-button
 export class AddToCalendarButton extends Component {
   constructor(props) {
@@ -8,14 +9,28 @@ export class AddToCalendarButton extends Component {
   }
 
   init() {
+    this.start = this.element.querySelector(".start");
+    this.end = this.element.querySelector(".end");
+    this.dateFormat = "YYYY-MM-DD";
+
     this.setStartDateTime = this.setStartDateTime.bind(this);
     this.setEndDateTime = this.setEndDateTime.bind(this);
+    this.setStartEndText = this.setStartEndText.bind(this);
+
+    this.setStartEndText();
+  }
+
+  setStartEndText() {
     this.setStartDateTime();
     this.setEndDateTime();
     window.addeventatc.refresh();
   }
 
-  setStartDateTime() {}
+  setStartDateTime() {
+    this.start.innerText = dayjs().add(7, "day").format(this.dateFormat);
+  }
 
-  setEndDateTime() {}
+  setEndDateTime() {
+    this.end.innerText = dayjs().add(8, "day").format(this.dateFormat);
+  }
 }
