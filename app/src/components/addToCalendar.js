679295f97ff9d287b02ea50e6b183c5536afea04
Chris Henrick <chrishenrick@gmail.com>
2020-11-02 08:14:51 -0800

    updated AddToCalendar:
    - use class methods for manipulating and setting start & end date
    - removes the need for using dayjs

--- app/src/components/addToCalendar.js
+++ app/src/components/addToCalendar.js
@@ -1,4 +1,3 @@
-import dayjs from "dayjs";
 import { Component } from "./_componentBase";
 
 // Wrapper component for add-to-calendar-button widget
@@ -16,6 +15,8 @@ export class AddToCalendar extends Component {
     this.setStartDateTime = this.setStartDateTime.bind(this);
     this.setEndDateTime = this.setEndDateTime.bind(this);
     this.setStartEndText = this.setStartEndText.bind(this);
+    this.daysFromNow = this.daysFromNow.bind(this);
+    this.formatDate = this.formatDate.bind(this);
 
     this.setStartEndText();
   }
@@ -27,10 +28,31 @@ export class AddToCalendar extends Component {
   }
 
   setStartDateTime() {
-    this.start.innerText = dayjs().add(7, "day").format(this.dateFormat);
+    this.start.innerText = this.formatDate(this.daysFromNow(7));
   }
 
   setEndDateTime() {
-    this.end.innerText = dayjs().add(8, "day").format(this.dateFormat);
+    this.end.innerText = this.formatDate(this.daysFromNow(8));
+  }
+
+  daysFromNow(days) {
+    if (days && typeof days === "number" && Math.floor(days) === days) {
+      const now = new Date();
+      return new Date(now.setDate(now.getDate() + days));
+    } else {
+      throw "daysFromNow's argument should be an integer";
+    }
+  }
+
+  formatDate(dateObj) {
+    if (dateObj && dateObj instanceof Date) {
+      return dateObj.toLocaleString("en-US", {
+        year: "numeric",
+        day: "2-digit",
+        month: "2-digit",
+      });
+    } else {
+      throw "formatDate's argument should be a Date object";
+    }
   }
 }
