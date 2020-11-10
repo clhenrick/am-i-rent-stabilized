1c639951c7baa0d29b9107f35368ca9e0dba7351
Chris Henrick <chrishenrick@gmail.com>
2020-11-09 20:42:01 -0800

    fixed LanguageToggle instance in initInfoPages

--- app/src/utils/initInfoPages.js
+++ app/src/utils/initInfoPages.js
@@ -1,7 +1,11 @@
+import { store } from "../store";
 import { NavMenuToggle } from "../components/navigation";
 import { LanguageToggle } from "../components/languageToggle";
 
 export default function initInfoPages() {
   new NavMenuToggle({ element: document.querySelector("nav.main-nav") });
-  new LanguageToggle({ element: document.querySelector("div.lang-toggle") });
+  new LanguageToggle({
+    element: document.querySelector("div.lang-toggle"),
+    store,
+  });
 }
