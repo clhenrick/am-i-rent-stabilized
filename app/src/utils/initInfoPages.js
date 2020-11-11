bea0eb99fb5298fd4b6b390f916b16f19e51dc9a
Chris Henrick <chrishenrick@gmail.com>
2020-11-11 06:38:57 -0800

    initInfoPages uses ComponentRegistry

--- app/src/utils/initInfoPages.js
+++ app/src/utils/initInfoPages.js
@@ -1,11 +1,21 @@
 import { store } from "../store";
+import { ComponentRegistry } from "./componentRegistry";
 import { NavMenuToggle } from "../components/navigation";
 import { LanguageToggle } from "../components/languageToggle";
 
+const registry = new ComponentRegistry();
+
 export default function initInfoPages() {
-  new NavMenuToggle({ element: document.querySelector("nav.main-nav") });
-  new LanguageToggle({
-    element: document.querySelector("div.lang-toggle"),
-    store,
-  });
+  registry.add(
+    "navMenuToggle",
+    new NavMenuToggle({ element: document.querySelector("nav.main-nav") })
+  );
+
+  registry.add(
+    "languageToggle",
+    new LanguageToggle({
+      element: document.querySelector("div.lang-toggle"),
+      store,
+    })
+  );
 }
