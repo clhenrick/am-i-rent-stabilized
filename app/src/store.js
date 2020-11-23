e3d66712271c97de133c5a05910905c118ee2129
Chris Henrick <chrishenrick@gmail.com>
2020-11-22 17:16:11 -0800

    added redux middleware for logging exceptions to GA

--- app/src/store.js
+++ app/src/store.js
@@ -2,6 +2,7 @@ import thunkMiddleware from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
 import { rootReducer } from "./reducers";
 import logger from "redux-logger";
+import { logException } from "./utils/logging";
 
 const USE_LOGGER = process.env.USE_REDUX_LOGGER;
 const middlewares = [thunkMiddleware];
@@ -14,8 +15,25 @@ const composeEnhancers =
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : compose;
 
+const crashReporter = () => (next) => (action) => {
+  try {
+    return next(action);
+  } catch (err) {
+    logException(
+      `Redux caught exception: ${
+        typeof err === "object" ? `${err.name}, ${err.message}` : err
+      }`
+    );
+    throw err;
+  }
+};
+
 if (process.env.NODE_ENV === "development" && USE_LOGGER) {
   middlewares.push(logger);
+}
+
+if (process.env.NODE_ENV === "production") {
+  middlewares.push(crashReporter);
 }
 
 export const store = createStore(
