3b740cae24525bb46cfc5b6cfb3adb3ebfdefba5
Chris Henrick <chrishenrick@gmail.com>
2020-11-23 20:05:56 -0800

    added tests for utils/logging

--- app/src/utils/logging.spec.js
+++ app/src/utils/logging.spec.js
@@ -1,20 +1,78 @@
-import { logEvent } from "./logging";
-const { logAddressSearch } = jest.requireActual("./logging");
-
-jest.mock("./logging");
+import {
+  logEvent,
+  logException,
+  logAddressSearch,
+  logAddressNF,
+  logAddressRS,
+  logLanguageToggle,
+  logAddressNotRS,
+  handleErrorObj,
+} from "./logging";
 
 describe("logging", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
+  test("logEvent", () => {
+    logEvent("Foo", { event_category: "Bla", label: "Bar" });
+    expect(gtag).toHaveBeenCalledWith("event", "Foo", {
+      event_category: "Bla",
+      label: "Bar",
+    });
+  });
+
+  test("logException", () => {
+    logException("Foo Bar", true);
+    expect(gtag).toHaveBeenCalledWith("event", "exception", {
+      description: "Foo Bar",
+      fatal: true,
+    });
+  });
+
   test("logAddressSearch", () => {
-    // for some reason the jest mock for logEvent is not being called below, I give up.
-    expect(true).toBe(true);
-    // logAddressSearch("Foo");
-    // expect(logEvent).toHaveBeenCalledWith("Address Search", {
-    //   event_category: "search",
-    //   label: "Foo",
-    // });
+    logAddressSearch("Foo");
+    expect(gtag).toHaveBeenCalledWith("event", "Address Search", {
+      event_category: "search",
+      label: "Foo",
+    });
+  });
+
+  test("logAddressNF", () => {
+    logAddressNF("Foo");
+    expect(gtag).toHaveBeenCalledWith("event", "Address Not Found", {
+      event_category: "search",
+      label: "Foo",
+    });
+  });
+
+  test("logAddressRS", () => {
+    logAddressRS("Foo");
+    expect(gtag).toHaveBeenCalledWith("event", "Address Rent Stabilized", {
+      event_category: "search",
+      label: "Foo",
+    });
+  });
+
+  test("logAddressNotRS", () => {
+    logAddressNotRS("Foo");
+    expect(gtag).toHaveBeenCalledWith("event", "Address Not Rent Stabilized", {
+      event_category: "search",
+      label: "Foo",
+    });
+  });
+
+  test("logLanguageToggle", () => {
+    logLanguageToggle("zh");
+    expect(gtag).toHaveBeenCalledWith("event", "Language Toggle", {
+      label: "zh",
+    });
+  });
+
+  test("handleErrorObj", () => {
+    let result = handleErrorObj("Foo", new Error("Bar"));
+    expect(result).toEqual("Foo; Error; Bar");
+    result = handleErrorObj("Foo", "Bar");
+    expect(result).toEqual("Foo; Bar");
   });
 });
