585aa400c95c4d0a7d9767430a56e6a9a478b8fc
Chris Henrick <chrishenrick@gmail.com>
2021-04-10 13:21:28 -0700

    added tests for tr groups reducer

--- app/src/reducers/tenantsRightsReducer.js
+++ app/src/reducers/tenantsRightsReducer.js
@@ -1,6 +1,6 @@
 import * as types from "../constants/actionTypes";
 
-const initialState = {
+export const initialState = {
   status: "idle",
   results: null,
   error: null,
@@ -11,7 +11,7 @@ export function tenantsRights(state = initialState, action) {
     case types.TenantsRightsRequest:
       return {
         ...state,
-        status: "pending",
+        status: "fetching",
       };
     case types.TenantsRightsSuccess:
       return {
