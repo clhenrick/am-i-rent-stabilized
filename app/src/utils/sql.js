29a6eb4448b85e3177fd7ab86491e70a59b7cbff
Chris Henrick <chrishenrick@gmail.com>
2021-04-09 19:54:54 -0700

    added sql query for tr search

--- app/src/utils/sql.js
+++ app/src/utils/sql.js
@@ -5,3 +5,21 @@ export const rentStabilizedBblSql = (bbl) =>
 
 export const mapsApiSql = () =>
   `SELECT the_geom_webmercator FROM ${rentStabilizedTable}`;
+
+export const tenantsRightsSearchSql = ({ lon, lat }) =>
+  `SELECT 
+    name, 
+    full_address, 
+    email, 
+    phone, 
+    description, 
+    service_area, 
+    website_url 
+  FROM nyc_tenants_rights_service_areas
+  WHERE ST_Contains(
+    the_geom,
+    ST_GeomFromText(
+      'POINT(' || ${lon} || ' ' || ${lat} ||  ')',
+      4326
+    )
+  )`;
