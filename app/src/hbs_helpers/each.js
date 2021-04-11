1ea69221efbbad69ad80191ae0080d15c8da2c28
Chris Henrick <chrishenrick@gmail.com>
2021-04-11 16:45:36 -0700

    rm unneeded handlebars helper modules

--- app/src/hbs_helpers/each.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (context, options) {
-  var ret = "";
-  for (var i = 0, j = context.length; i < j; i++) {
-    ret = ret + options.fn(context[i]);
-  }
-  return ret;
-};
--- app/src/hbs_helpers/if.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (conditional, options) {
-  if (conditional) {
-    return options.fn(this);
-  } else {
-    return options.inverse(this);
-  }
-};
