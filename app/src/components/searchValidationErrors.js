e264631ec242cdd2c1db70679338a9cf0ba7f6ab
Chris Henrick <chrishenrick@gmail.com>
2023-12-09 15:49:56 -0800

    set aria-describedby on form input to error msg when error occurs

--- app/src/components/searchValidationErrors.js
+++ app/src/components/searchValidationErrors.js
@@ -1,11 +1,24 @@
 import { Component } from "./_componentBase";
 
+/** HTML id attribute values for validation text */
+const VALIDATION_TEXT_ID = {
+  NOT_FOUND: "error-not-found",
+  NO_INPUT: "error-address",
+  GENERIC: "error-generic",
+};
+
 export class SearchValidationErrors extends Component {
   init({ searchForm }) {
     this._searchForm = searchForm;
-    this.errorNotFound = this.element.querySelector("#error-not-found");
-    this.errorNoInput = this.element.querySelector("#error-address");
-    this.errorGeneric = this.element.querySelector("#error-generic");
+    this.errorNotFound = this.element.querySelector(
+      `#${VALIDATION_TEXT_ID.NOT_FOUND}`
+    );
+    this.errorNoInput = this.element.querySelector(
+      `#${VALIDATION_TEXT_ID.NO_INPUT}`
+    );
+    this.errorGeneric = this.element.querySelector(
+      `#${VALIDATION_TEXT_ID.GENERIC}`
+    );
   }
 
   showAll() {
@@ -22,30 +35,45 @@ export class SearchValidationErrors extends Component {
 
   showNotFound() {
     this.errorNotFound.classList.remove("hidden");
+    this._searchForm.inputAddress.setAttribute(
+      "aria-describedby",
+      VALIDATION_TEXT_ID.NOT_FOUND
+    );
   }
 
   hideNotFound() {
     this.errorNotFound.classList.add("hidden");
+    this._searchForm.inputAddress.removeAttribute("aria-describedby");
   }
 
   showNoInput() {
     this.errorNoInput.classList.remove("hidden");
+    this._searchForm.inputAddress.setAttribute(
+      "aria-describedby",
+      VALIDATION_TEXT_ID.NO_INPUT
+    );
     this._searchForm.inputAddress.classList.add("invalid");
     this._searchForm.inputAddress.setAttribute("aria-invalid", true);
   }
 
   hideNoInput() {
     this.errorNoInput.classList.add("hidden");
+    this._searchForm.inputAddress.removeAttribute("aria-describedby");
     this._searchForm.inputAddress.classList.remove("invalid");
     this._searchForm.inputAddress.setAttribute("aria-invalid", false);
   }
 
   showGeneric() {
     this.errorGeneric.classList.remove("hidden");
+    this._searchForm.inputAddress.setAttribute(
+      "aria-describedby",
+      VALIDATION_TEXT_ID.GENERIC
+    );
   }
 
   hideGeneric() {
     this.errorGeneric.classList.add("hidden");
+    this._searchForm.inputAddress.removeAttribute("aria-describedby");
   }
 
   get noInputIsHidden() {
