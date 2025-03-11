914b6322523150ba09ab5b4f63aba357a509d46b
Chris Henrick <chrishenrick@gmail.com>
2025-03-11 10:01:13 -0700

    updated rentStabilizedBblSql to use new carto table name

--- app/src/utils/sql.js
+++ app/src/utils/sql.js
@@ -1,20 +1,23 @@
-import { rentStabilizedTable } from "../constants/config";
+import {
+  rentStabilizedTable,
+  cartoV3RentStabilizedTableName,
+} from "../constants/config";
 
 export const rentStabilizedBblSql = (bbl) =>
-  `SELECT bbl FROM ${rentStabilizedTable} WHERE bbl = ${bbl}`;
+  `SELECT bbl FROM ${cartoV3RentStabilizedTableName} WHERE bbl = ${bbl}`;
 
 export const mapsApiSql = () =>
   `SELECT the_geom_webmercator FROM ${rentStabilizedTable}`;
 
 export const tenantsRightsGroupsSql = ({ lon, lat }) =>
-  `SELECT 
-    name, 
-    full_address, 
-    email, 
-    phone, 
-    description, 
-    service_area, 
-    website_url 
+  `SELECT
+    name,
+    full_address,
+    email,
+    phone,
+    description,
+    service_area,
+    website_url
   FROM nyc_tenants_rights_service_areas
   WHERE ST_Contains(
     the_geom,
