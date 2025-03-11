0122ef9f0f0f7686a79ee458de75928a67ef7b7c
Chris Henrick <chrishenrick@gmail.com>
2025-03-11 09:51:36 -0700

    updated config with carto v3 vars

--- app/src/constants/config.js
+++ app/src/constants/config.js
@@ -2,3 +2,11 @@
 export const cartoAccount = "chenrick";
 export const rentStabilizedTable = "mappluto_likely_rs_2020_v8";
 export const geosearchApiVersion = "v2";
+
+export const cartoAPIv3BaseURL = "https://gcp-us-east1.api.carto.com";
+export const cartoV3RentStabilizedTableName =
+  "shared.mappluto_likely_rs_2020_v8";
+
+// NOTE: this API key is restricted for use on the domain https://amirentstabilized.com
+export const cartoApiKey =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhIjoiYWNfd3cwaDFiczgiLCJqdGkiOiIyOTVmZGRkNiJ9.w6GC-oesQEl5HerTbTlZWxFb8-c8iX7F0NAG9pCunu0";
