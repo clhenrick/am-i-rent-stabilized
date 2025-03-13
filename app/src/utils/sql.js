8a58317738f43f8a7ca72535bb2d86ab0c40dccd
Chris Henrick <chrishenrick@gmail.com>
2025-03-13 15:53:05 -0700

    #150 added sql for querying likely rs geojson

--- app/src/utils/sql.js
+++ app/src/utils/sql.js
@@ -4,11 +4,18 @@ import {
   cartoV3TenantsRightsServiceAreasTable,
 } from "../constants/config";
 
+const searchResultBufferDistance = 500; // meters
+
 export const rentStabilizedBblSql = (bbl) =>
   `SELECT bbl FROM ${cartoV3RentStabilizedTableName} WHERE bbl = ${bbl}`;
 
 export const mapsApiSql = () =>
   `SELECT the_geom_webmercator FROM ${rentStabilizedTable}`;
+
+export const rentStabilizedGeomSql = ({ lon, lat }) =>
+  `SELECT ST_ASGEOJSON(geom) as geojson
+		FROM ${cartoV3RentStabilizedTableName}
+		WHERE ST_DWithin(geom, ST_GeogFromText('POINT(${lon} ${lat})'), ${searchResultBufferDistance})`;
 
 export const tenantsRightsGroupsSql = ({ lon, lat }) =>
   `SELECT
