5e6d24ff5ff3e3b5008565d7f820ea3cc5e7a668
Chris Henrick <chrishenrick@gmail.com>
2025-03-15 10:40:18 -0700

    #150 updated sql string tests

--- app/src/utils/sql.js
+++ app/src/utils/sql.js
@@ -10,9 +10,9 @@ export const rentStabilizedBblSql = (bbl) =>
   `SELECT bbl FROM ${cartoV3RentStabilizedTableName} WHERE bbl = ${bbl}`;
 
 export const rentStabilizedGeomSql = ({ lon, lat }) =>
-  sls`SELECT ST_ASGEOJSON(geom) as geojson
-		FROM ${cartoV3RentStabilizedTableName}
-		WHERE ST_DWithin(
+  sls`SELECT ST_AsGeoJSON(geom) as geojson
+    FROM ${cartoV3RentStabilizedTableName}
+    WHERE ST_DWithin(
       geom,
       ST_GeogFromText(
         'POINT(' || ${lon} || ' ' || ${lat} ||')'
--- app/src/utils/sql.spec.js
+++ app/src/utils/sql.spec.js
@@ -1,11 +1,30 @@
 import * as sql from "./sql";
-import { cartoV3RentStabilizedTableName } from "../constants/config";
+import {
+  cartoV3RentStabilizedTableName,
+  cartoV3TenantsRightsServiceAreasTable,
+} from "../constants/config";
 
 describe("utils/sql", () => {
   test("rentStabilizedBblSql", () => {
     const result = sql.rentStabilizedBblSql("999999999");
     expect(result).toBe(
       `SELECT bbl FROM ${cartoV3RentStabilizedTableName} WHERE bbl = 999999999`
+    );
+  });
+
+  test("rentStabilizedGeomSql", () => {
+    const [lon, lat] = [-73, 40];
+    const result = sql.rentStabilizedGeomSql({ lon, lat });
+    expect(result).toBe(
+      `SELECT ST_AsGeoJSON(geom) as geojson FROM ${cartoV3RentStabilizedTableName} WHERE ST_DWithin( geom, ST_GeogFromText( 'POINT(' || ${lon} || ' ' || ${lat} ||')' ), 500 )`
+    );
+  });
+
+  test("tenantsRightsGroupsSql", () => {
+    const [lon, lat] = [-73, 40];
+    const result = sql.tenantsRightsGroupsSql({ lon, lat });
+    expect(result).toBe(
+      `SELECT name, full_address, email, phone, description, service_area, website_url FROM ${cartoV3TenantsRightsServiceAreasTable} WHERE ST_Contains( geom, ST_GeogFromText( 'POINT(' || ${lon} || ' ' || ${lat} ||  ')' ) )`
     );
   });
 });
