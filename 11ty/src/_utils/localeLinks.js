757c81e7a3d4e7dbf9acf995ff9586be51f29cd9
Chris Henrick <chrishenrick@gmail.com>
2025-05-11 17:23:12 -0700

    fixed getRoutesForLang util to handle index pages

--- 11ty/src/_utils/localeLinks.js
+++ 11ty/src/_utils/localeLinks.js
@@ -1,5 +1,5 @@
 const langCodes = ["en", "es", "zh"];
-const langLabels = ["In English", "En espanol", "中文"];
+const langLabels = ["in english", "en español", "中文"];
 const langData = langCodes.reduce((acc, cur, index) => {
   acc.set(cur, langLabels[index]);
   return acc;
@@ -7,15 +7,19 @@ const langData = langCodes.reduce((acc, cur, index) => {
 
 /** generates the necessary data for each page to link to its equivalent page in the other supported languages */
 export function getRoutesForLang (currentLang, page, isHomepage = false) {
+  // homepages are the only page that uses "index.html" as the file name, 11ty page data will show fileSlug as an empty string, so we correct it here
+  if (isHomepage) {
+    page = "index";
+  }
   return [...langData.entries()]
     .filter(([lang]) => lang !== currentLang)
     .map(([lang, label], i) => {
-      const toReturn = { label, lang };
+      const datum = { label, lang };
       if (lang === "en" && isHomepage) {
-        toReturn.path = `/${page}.html`;
+        datum.path = `/${page}.html`;
       } else {
-        toReturn.path = `/${lang}/${page}.html`;
+        datum.path = `/${lang}/${page}.html`;
       }
-      return toReturn;
+      return datum;
     });
 }
