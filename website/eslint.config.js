5143e0d3d3c82d5dcb84f8504ecfb4d1ada697a5
Chris Henrick <chrishenrick@gmail.com>
2025-05-25 21:14:36 -0700

    updated eslint config: use eslint-config-prettier

--- website/eslint.config.js
+++ website/eslint.config.js
@@ -2,10 +2,25 @@ import js from "@eslint/js";
 import globals from "globals";
 import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
-
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node, ...globals.jest} } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, ...globals.jest },
+    },
+  },
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  eslintConfigPrettier,
 ]);
