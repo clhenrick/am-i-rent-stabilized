eb505b5ca373419d22d4d875e1ba47bed614cb6f
Chris Henrick <chrishenrick@gmail.com>
2025-06-03 20:39:55 -0700

    updated stylelint config:
    - disabled rule scss/at-extend-no-missing-placeholder
    - disabled rule selector-class-pattern

--- website/stylelint.config.js
+++ website/stylelint.config.js
@@ -1,6 +1,10 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ["stylelint-config-standard-scss"],
+  rules: {
+    "scss/at-extend-no-missing-placeholder": null,
+    "selector-class-pattern": null,
+  },
   reportDescriptionlessDisables: true,
   reportInvalidScopeDisables: true,
   reportNeedlessDisables: true,
