a3cc6b6cf396246b6dce70c2100ba4007af5fe47
Chris Henrick <chrishenrick@gmail.com>
2026-04-01 17:45:22 -0700

    fix(data): use subsidies sql to import data to postgres

--- data/db-service/init-db.sh
+++ data/db-service/init-db.sh
@@ -3,8 +3,7 @@
 set -e
 
 psql -d $POSTGRES_DB -U $POSTGRES_USER -f /home/data/mappluto.sql
-psql -d $POSTGRES_DB -U $POSTGRES_USER -f /home/data/421a_exemptions.sql
-psql -d $POSTGRES_DB -U $POSTGRES_USER -f /home/data/j51_exemptions.sql
+psql -d $POSTGRES_DB -U $POSTGRES_USER -f /home/data/subsidies.sql
 psql -d $POSTGRES_DB -U $POSTGRES_USER -f /home/data/likely-rent-stabilized.sql
 
 exit
